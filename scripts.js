dd6716caf2774e3327742fcaa67ecc013434f448
Diogo Oliveira Vieira <ddiogooliveira9@gmail.com>
2024-07-27 15:23:13 -0300

    Getting the selected currency

--- scripts.js
+++ scripts.js
@@ -1,4 +1,7 @@
+// Getting the form elements.
+const form = document.querySelector("form")
 const amount = document.getElementById("amount")
+const currency = document.getElementById("currency")
 
 
 // Manipulate the input value to receive only numbers.
@@ -6,3 +9,10 @@ amount.addEventListener("input", () => {
     const hasCharactersRegex = /\D+/g
     amount.value = amount.value.replace(hasCharactersRegex, "")
 })
+
+// Capturing the form submit event.
+form.onsubmit = () => {
+    event.preventDefault()
+
+    console.log(currency.value)
+}
