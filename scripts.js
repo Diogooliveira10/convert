fe392992d6fac6dec3a9f8d7ad73d08be1ba8295
Diogo Oliveira Vieira <ddiogooliveira9@gmail.com>
2024-07-27 15:36:19 -0300

    Identifying the currency, price and symbol

--- scripts.js
+++ scripts.js
@@ -1,3 +1,8 @@
+// Currency quote of the day.
+const USD = 5.64
+const EUR = 6.14
+const GBP = 7.26
+
 // Getting the form elements.
 const form = document.querySelector("form")
 const amount = document.getElementById("amount")
@@ -14,5 +19,20 @@ amount.addEventListener("input", () => {
 form.onsubmit = () => {
     event.preventDefault()
 
-    console.log(currency.value)
+    switch (currency.value) {
+        case "USD":
+            convertCurrency(amount.value, USD, "US$")
+            break
+        case "EUR":
+            convertCurrency(amount.value, EUR, "€")
+            break
+        case "GBP":
+            convertCurrency(amount.value, GBP, "£")
+            break
+    }
+}
+
+// Function to convert currency.
+function convertCurrency(amount, price, symbol) {
+    console.log(amount, price, symbol)
 }
