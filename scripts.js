39094046cc585e1214dcbc96eecc3ef418f9e976
Diogo Oliveira Vieira <ddiogooliveira9@gmail.com>
2024-07-27 15:57:31 -0300

    Displaying the currency quote

--- scripts.js
+++ scripts.js
@@ -8,6 +8,7 @@ const form = document.querySelector("form")
 const amount = document.getElementById("amount")
 const currency = document.getElementById("currency")
 const footer = document.querySelector("main footer")
+const description = document.getElementById("description")
 
 
 // Manipulate the input value to receive only numbers.
@@ -36,12 +37,16 @@ form.onsubmit = () => {
 // Function to convert currency.
 function convertCurrency(amount, price, symbol) {
     try {
+        description.textContent = `${symbol} 1 = ${price}`
+
+
         // Applying the class that displays the footer to show the result.
         footer.classList.add("show-result")
+
     } catch (error) {
         // Removing the footer class that is hidden.
         footer.classList.remove("show-result")
-        
+
         console.log(error)
         alert("Unable to convert!")
         
